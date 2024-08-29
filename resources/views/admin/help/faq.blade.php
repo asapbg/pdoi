69c75b2c88a60700697c44518d2a141cebb2f80b
ASAP <noreply@asap.bg>
2024-08-29 16:49:50 +0300

    fix admin help page files

--- resources/views/admin/help/faq.blade.php
+++ resources/views/admin/help/faq.blade.php
@@ -14,7 +14,7 @@
         @if($page->files->count())
             @foreach($page->files as $f)
                 <div class="row p-3">
-                    <a class="w-100 mb-2" target="_blank" href="{{ route('admin.download.file', $f) }}">
+                    <a class="w-100 mb-2" target="_blank" href="{{ route('download.page.file', ['file' => $f->id]) }}">
                         {!! fileIcon($f->content_type) !!} {{ $f->description }}
                     </a>
                 </div>
