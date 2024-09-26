ab951e455efff1ac5eb006d34ca9225f658b9ab4
ASAP <noreply@asap.bg>
2024-09-26 16:13:42 +0300

    PITAY-82

--- resources/views/admin/applications/log_view_notification.blade.php
+++ resources/views/admin/applications/log_view_notification.blade.php
@@ -16,7 +16,7 @@
                         </div>
                         <div class="form-group form-group-sm col-12 mb-3">
                             <label class="form-label fw-semibold" >Дата/час на настъпване:</label>
-                            <span>{{ displayDateTime($notification->created_at }}</span>
+                            <span>{{ displayDateTime($notification->created_at) }}</span>
                         </div>
                         <div class="form-group form-group-sm col-12 mb-3">
                             <label class="form-label fw-semibold" >Статус:</label>
